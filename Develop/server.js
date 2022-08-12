b6075c6dacda034337398cf4944f5238284288f9
Kammin Avery <kammin.avery@gmail.com>
2022-08-11 21:44:56 -0600

    moved server.js to the right place...

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/note.js');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+// Get routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users back to home page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
