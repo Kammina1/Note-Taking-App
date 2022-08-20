b24248d148e9240cd1ab21634bd2f2ff3ffe57cf
Kammin Avery <kammin.avery@gmail.com>
2022-08-20 16:16:43 -0600

    finished the routes/project

--- Develop/routes/note.js
+++ Develop/routes/note.js
@@ -1,85 +1,89 @@
 const note = require("express").Router();
 const { notEqual } = require("assert");
-// const express = reqire('express');
-const fs = require("fs").promises;
+const { json, response } = require("express");
+const fs = require("fs");
+const fsp = require("fs").promises;
+const { request } = require("http");
+const { v4: uuidv4 } = require("uuid");
 
-async function getFile(note) {
-    const data = await fs.readFile("./db/db.json", "binary");
-    const json = JSON.parse(Buffer.from(data).toString())
-    console.log(Object.getOwnPropertyNames(json));
-    json.push(note);
-    console.log(json);
-//     return Buffer.from(data);
-//     const data = await readFile("./db/db.json", function (err, data) {
-//         json = JSON.parse(data)
-//        console.log(json);
-//        json.push(note)
-//        fs.writeFile("./db/db.json", JSON.stringify(json), function(err) {
-//            if (err) {
-//              throw err;
-//            }
-//          });
-//    })
-}
-
-note.post("/", (req, res) => {
-  const { title, text } = req.body;
-  console.log(req.body);
-  if (!title || !text) {
-    return res.statusCode(400).json({ msg: "Enter a title/Text" });
-  }
-  const note = {
-    title,
-    text
-  };
-  try {
-    let json;
-    getFile(note);
-  }
-   catch (err) {
-    console.log(err);
-    res.status(500);
-  }
-  res.sendStatus(200);
+// GET Route for retrieving all the feedback
+note.get("/", (req, res) => {
+  let json = "";
+  fs.readFile("./db/db.json", function (err, data) {
+    json = JSON.parse(data);
+    res.json(json);
+  });
 });
 
+//Get Route for retrieving individual note
+note.get("/:id", async (req, res) => {
+  if (req.params.id) {
+    const data = await fsp.readFile("./db/db.json", "binary");
+    let notes = JSON.parse(Buffer.from(data));
+    var resNote = null;
+    notes.forEach(note => {
+        if (note.id === req.params.id) {
+        resNote = note;
+        }
+    })
+    if (resNote) {
+      res.json(resNote);
+    } else {
+      res.status(404).json({ msg: "Id not found!" });
+    }
+  } else {
+    res.status(400).json({ msg: "Request expects an ID!" });
+  }
+});
+
+//Post Route for creating a note
+note.post("/", (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    res.status(400).json({ msg: "Enter a title/Text" });
+  } else {
+    const note = {
+      id: uuidv4(),
+      title,
+      text,
+    };
+    try {
+      fs.readFile("./db/db.json", function (err, data) {
+        let json = JSON.parse(data);
+        json.push(note);
+        fs.writeFile("./db/db.json", JSON.stringify(json), function (err) {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500);
+    }
+    res.status(200).send("Success!!!");
+  }
+});
+
+note.delete('/:id', async (req,res) => {
+    if (req.params.id) {
+        const data = await fsp.readFile("./db/db.json", "binary");
+        let notes = JSON.parse(Buffer.from(data));
+        var newNotes = notes.filter(note => note.id !== req.params.id)
+        try {
+            fs.writeFile("./db/db.json", JSON.stringify(newNotes), function (err) {
+                if (err) {
+                  throw err;
+                }
+              });
+          } catch (err) {
+            console.log(err);
+            res.status(500);
+          }
+          res.status(200).json({msg: "Successfully deleted!!!"});
+      } else {
+        res.status(400).json({ msg: "Request expects an ID!" });
+      }
+    });
+
 module.exports = note;
-
-// const fb = require('express').Router();
-// const { v4: uuidv4 } = require('uuid');
-// const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
-
-// // GET Route for retrieving all the feedback
-// fb.get('/', (req, res) =>
-//   readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
-// );
-
-// // POST Route for submitting feedback
-// fb.post('/', (req, res) => {
-//   // Destructuring assignment for the items in req.body
-//   const { email, feedbackType, feedback } = req.body;
-
-//   // If all the required properties are present
-//   if (email && feedbackType && feedback) {
-//     // Variable for the object we will save
-//     const newFeedback = {
-//       email,
-//       feedbackType,
-//       feedback,
-//       feedback_id: uuidv4(),
-//     };
-
-//     readAndAppend(newFeedback, './db/feedback.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newFeedback,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting feedback');
-//   }
-// });
-
-// module.exports = fb;
